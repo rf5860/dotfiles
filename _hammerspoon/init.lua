6dc66d1efc75852d1b88323dd5f6cb4cfcb8efef
Glenn Waters <gwwaters@gmail.com>
2016-06-30 20:05:10 -0400

    Add caffienate icon to menubar

--- _hammerspoon/init.lua
+++ _hammerspoon/init.lua
@@ -39,14 +39,21 @@ end
 appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()
 
+local caffeine = hs.menubar.new()
+caffeine:removeFromMenuBar()
+caffeine:setIcon("caffeine-on.pdf")
+
 function caffeinate()
   hs.caffeinate.toggle("displayIdle")
   if hs.caffeinate.get("displayIdle") then
+    caffeine:returnToMenuBar()
     hs.alert.show("Staying AWAKE!", 2)
   else
+    caffeine:removeFromMenuBar()
     hs.alert.show("Sleepy", 2)
   end
 end
+caffeine:setClickCallback(caffeinate)
 
 -- Cycle args for function when called repeatedly: cycleCalls( fn, { {args1...}, ... } )
 function cycleCalls( fn, args )
